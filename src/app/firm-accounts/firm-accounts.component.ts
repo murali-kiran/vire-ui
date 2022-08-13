e2f3d19e6a7864f91e7850e25441dc2dd2b50bf8
Sravan13 <chithari.sravan@gmail.com>
2022-08-13 22:59:51 +0530

    Firm Profiles Implementation

--- src/app/firm-accounts/firm-accounts.component.ts
+++ src/app/firm-accounts/firm-accounts.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Profile } from './../../model/profile';
+import { UserService } from './../../service/user-service.service';
 
 @Component({
   selector: 'app-firm-accounts',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirmAccountsComponent implements OnInit {
 
-  constructor() { }
+  profiles : Profile [];
+  selectedProfile ?: Profile;
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.retriveAllusers();
+  }
+
+  retriveAllusers = ()=>{
+    this.userService.getFirmUsers('profileType:firm').subscribe(data=>{
+      this.profiles=data;
+    });
+  }
+
+  onItemChange = (profile : Profile) => {
+    this.selectedProfile = profile;
+    console.log(profile);
+  }
+
+  onBlockUser = (profile : Profile, isBlocked: boolean)=>{
+    this.userService.blockUser(profile.profileId,isBlocked).subscribe(data=>{
+
+      if(data && this.selectedProfile)
+        this.selectedProfile.isBlocked = !this.selectedProfile.isBlocked;
+      
+    });
+  }
+
+  onDeleteUser = (profile : Profile)=>{
+
+    this.userService.deleteUser(profile.profileId).subscribe(data=>{
+      this.retriveAllusers();
+      this.selectedProfile = undefined;
+    });
+
   }
 
 }
